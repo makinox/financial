72bcc158412dfa54c5f51285da9773514dc9f7f4
Jesus bossa <x@jesusbossa.com>
2020-04-09 10:54:18 -0500

    [ADD] Table dynamic

---
+++ PlatziFinanzas/App/Main/Transactions/TransactionsViewController.swift
@@ -0,0 +1,64 @@
+//
+//  TransactionsViewController.swift
+//  PlatziFinanzas
+//
+//  Created by Jesus Bossa on 8/04/20.
+//  Copyright © 2020 Jesus Bossa. All rights reserved.
+//
+
+import UIKit
+
+class TransactionsViewController: UIViewController {
+    @IBOutlet weak var tableView: UITableView!
+    
+    fileprivate(set) lazy var emptyStateView: UIView = {
+        guard let view = Bundle.main.loadNibNamed("EmptyState", owner: nil, options: [:])?.first as? UIView else {
+            return UIView()
+        }
+        return view
+    }()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+//        tableView.delegate = self
+//        tableView.dataSource = self
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
+
+extension TransactionsViewController: UITableViewDelegate {
+    
+}
+
+extension TransactionsViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        let count = 0
+//        tableView.backgroundView = (count == 0) ? emptyStateView : nil
+        if (count == 0) {
+            tableView.backgroundView = emptyStateView
+            tableView.separatorStyle = .none
+        } else {
+            tableView.backgroundView = nil
+            tableView.separatorStyle = .singleLine
+        }
+        return count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+    }
+    
+    
+}
